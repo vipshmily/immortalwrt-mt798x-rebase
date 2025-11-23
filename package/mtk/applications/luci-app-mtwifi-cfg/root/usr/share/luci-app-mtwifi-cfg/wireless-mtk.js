c388d74cdc1a48270d4ca6ad9bc2d64ce1759814
chasey-dev <ellenyoung0912@gmail.com>
2025-11-23 20:03:01 +0800

    mtk: mtwifi-cfg: luci app: fix broken wireless icon - since png resources were deprecated in new luci, we use svg resources instead.
    Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

--- package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
+++ package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
@@ -28,27 +28,22 @@ function count_changes(section_id) {
 
 function render_radio_badge(radioDev) {
 	return E('span', { 'class': 'ifacebadge' }, [
-		E('img', { 'src': L.resource('icons/wifi%s.png').format(radioDev.isUp() ? '' : '_disabled') }),
+		E('img', { 'src': L.resource('icons/wifi%s.svg').format(radioDev.isUp() ? '' : '_disabled') }),
 		' ',
 		radioDev.getName()
 	]);
 }
 
 function render_signal_badge(signalPercent, signalValue, noiseValue, wrap, mode) {
-	var icon, title, value;
+	let icon = L.resource('icons/signal-075-100.svg'), title, value;
 
-	if (signalPercent < 0)
-		icon = L.resource('icons/signal-none.png');
-	else if (signalPercent == 0)
-		icon = L.resource('icons/signal-0.png');
-	else if (signalPercent < 25)
-		icon = L.resource('icons/signal-0-25.png');
-	else if (signalPercent < 50)
-		icon = L.resource('icons/signal-25-50.png');
-	else if (signalPercent < 75)
-		icon = L.resource('icons/signal-50-75.png');
-	else
-		icon = L.resource('icons/signal-75-100.png');
+	switch(true) {
+	case(signalPercent  < 0): icon = L.resource('icons/signal-none.svg'); 	break;
+	case(signalPercent == 0): icon = L.resource('icons/signal-000-000.svg');		break;
+	case(signalPercent < 25): icon = L.resource('icons/signal-000-025.svg'); 	break;
+	case(signalPercent < 50): icon = L.resource('icons/signal-025-050.svg');	break;
+	case(signalPercent < 75): icon = L.resource('icons/signal-050-075.svg');	break;
+	}
 
 	if (signalValue != null && signalValue != 0) {
 		if (noiseValue != null && noiseValue != 0) {
@@ -719,7 +714,7 @@ return view.extend({
 					'data-ssid': bss.network.getSSID()
 				}, [
 					E('img', {
-						'src': L.resource('icons/wifi%s.png').format(bss.network.isUp() ? '' : '_disabled'),
+						'src': L.resource('icons/wifi%s.svg').format(bss.network.isUp() ? '' : '_disabled'),
 						'title': bss.radio.getI18n()
 					}),
 					E('span', [
