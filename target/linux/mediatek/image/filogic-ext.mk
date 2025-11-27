51c6699a04893ffe3f115c9e8d049bb426ce8089
chasey-dev <ellenyoung0912@gmail.com>
2025-11-27 15:51:37 +0800

    mediatek: filogic: add MTK U-Boot layout support for Netcore N60 Pro
    Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -14,3 +14,20 @@ define Device/xiaomi_mi-router-ax3000t-mtkuboot
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += xiaomi_mi-router-ax3000t-mtkuboot
+
+define Device/netcore_n60-pro-mtkuboot
+  DEVICE_VENDOR := Netcore
+  DEVICE_MODEL := N60 Pro
+  DEVICE_VARIANT := (MTK U-Boot layout)
+  DEVICE_DTS := mt7986a-netcore-n60-pro-mtkuboot
+  DEVICE_DTS_DIR := ../dts-ext
+  DEVICE_PACKAGES := kmod-usb3 automount
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += netcore_n60-pro-mtkuboot
